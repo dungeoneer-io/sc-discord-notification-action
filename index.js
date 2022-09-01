bfacc19b0378754c50e6e7f98321c0de0231bb18
Chris Creighton (@creighcl) <27293513+Creighcl@users.noreply.github.com>
2022-09-01 01:46:37 -0400

    v1.1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-
-try {
-  const who = core.getInput('who');
-  const message = core.getInput('message');
-  console.log(`Hello ${who} says ${message}`);
+const axios = require('axios');
 
 
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function doAction() {
+  try {
+    const message = core.getInput('message');
+
+    const url = secrets.DISCORD_NOTIFICATION_WEBHOOK;
+    await axios({ url, method: 'POST', data: message });
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
+
+doAction();
